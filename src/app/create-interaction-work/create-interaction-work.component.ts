08299b15c58a13312a67a87e94598773b4274d2b
BetaHsu <ychsu6-c@my.cityu.edu.hk>
2023-03-30 15:47:06 +0200

    30.3.2023

--- src/app/create-interaction-work/create-interaction-work.component.ts
+++ src/app/create-interaction-work/create-interaction-work.component.ts
@@ -1,22 +1,25 @@
-import { Component, EventEmitter, Output  } from '@angular/core';
+import { Component, EventEmitter, Output, Input  } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { getParagraph } from '../services/database-service';
 
 @Component({
   selector: 'app-create-interaction-work',
   templateUrl: './create-interaction-work.component.html',
-  styleUrls: ['./create-interaction-work.component.css']
+  styleUrls: ['./create-interaction-work.component.css'],
 })
 export class CreateInteractionWorkComponent {
   title = 'fyp';
   workTitle: string = 'A long journey from bush to concrete';
+  user: string | undefined = undefined;
   i = 0;
+
+  // var for drop down list to change view
   view = 0;
   views = ['Interaction', 'Community', 'Public', 'Owner'];
   dropdownActive = false;
-  user: string | undefined = undefined;
+
+  // var for Drag and Drop input fields
   input: string = " ";
   displayVal= '';
   displayTitle = '';
@@ -24,13 +27,39 @@ export class CreateInteractionWorkComponent {
   rewritingSection2 = '';
   sorted = false;
   inputTextResorted : string[] = [];
+  disabledItemId = 'text2';
+  buttonText: string = 'Save';
+  items = [
+    { id: 'text1', rows: '4', cols: '73', placeholder: 'Enter 1st section of rewriting work', text: this.rewritingSection1, disabled: false  },
+    { id: 'text2', rows: '4', cols: '73', text: this.workTitle, disabled: true },
+    { id: 'text3', rows: '4', cols: '73', placeholder: 'Enter 2nd section of rewriting work', text: this.rewritingSection2, disabled: false }
+  ];
+  // @ViewChild('resortableList') resortableList: any;
+  // hideInputFields = false;
+
+  // var for fetching paragraphs from database
+  paragraph: any = undefined;
+  output: string = '';
   @Output() viewSelected = new EventEmitter<string>();
 
-  items = [
-    { id: 'text1', rows: '8', cols: '38', placeholder: 'Enter 1st section of rewriting work', text: this.rewritingSection1 },
-    { id: 'text2', rows: '1', cols: '38', text: this.workTitle },
-    { id: 'text3', rows: '8', cols: '38', placeholder: 'Enter 2nd section of rewriting work', text: this.rewritingSection2 }
+  // var for testing revealing & hidding sections of paragraph
+  text = "Through decades that ran like rivers, endless rivers of endless woes. Through pick and shovel sjambok and jail. O such a long long journey! When the motor-car came, the sledge and the ox-cart began to die. But for a while the bicycle made"
+  metadata = [
+    { index_interval_start: 0, index_interval_end: 45, revealed_score: 1 },
+    { index_interval_start: 45, index_interval_end: 80, revealed_score: 0 }
   ];
+  
+  ngOnInit(): void {
+    this.getParagraph();
+    this.generateParagraph();
+  }
+
+  generateParagraph() {
+    this.metadata.forEach((data) => {
+      this.output += '<span class=' + (data.revealed_score ? "substring--visible" : "substring--hidden") + '>' + this.text.substring(data.index_interval_start, data.index_interval_end) + '</span>'
+    })
+    console.log(this.output)
+  }
 
   toggleDropdown() {
     // Toggle the dropdown
@@ -39,13 +68,23 @@ export class CreateInteractionWorkComponent {
   }
 
   getParagraph() {
-    console.log(getParagraph())
+    //console.log(getParagraph())
+    console.log('Getting paragraph')
+    fetch("/api/v1/get-paragraph", {
+        method: 'GET',
+        mode: 'cors'
+    }
+    )
+    .then((response) => response.json())
+    .then((data => {
+      this.paragraph = data;
+      console.log(data)
+    }));
   }
 
   setView(newView: number) {
     // Close the dropdown
     this.dropdownActive = false
-
     // Update the view
     this.view = newView
   }
@@ -53,33 +92,50 @@ export class CreateInteractionWorkComponent {
   drop(event: CdkDragDrop<any[]>) {
     const draggedItem = this.items[event.previousIndex];
     const targetItem = this.items[event.currentIndex];
-
+    const movedItem = this.items[event.previousIndex];
+    if (movedItem.disabled) {
+      return;
+    }
     // Remove the dragged item from the array
     this.items.splice(event.previousIndex, 1);
     // Insert the dragged item at the target index
     this.items.splice(event.currentIndex, 0, draggedItem);
 
-    // If the target item is defined, swap text values of the dragged item and the target item
-    if (targetItem) {
-      const temp = draggedItem.text;
-      draggedItem.text = targetItem.text;
-      targetItem.text = temp;
-    }
     moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+
+    // If target item is defined, swap properties of the dragged item and the target item
+    if (targetItem) {
+      // swap text value
+      const tempText = draggedItem.text;
+      draggedItem.text = targetItem.text;
+      targetItem.text = tempText;
+      // swap placeholder
+      const tempPlaceholder = draggedItem.placeholder;
+      draggedItem.placeholder = targetItem.placeholder;
+      targetItem.placeholder = tempPlaceholder;
+      // swap disabled property
+      const tempDisabled = draggedItem.disabled;
+      draggedItem.disabled = targetItem.disabled;
+      targetItem.disabled = tempDisabled;
+    }
     this.sorted = true;
   }
 
+  // if click button, get resorted texts, update button text, disable edit for text fields
   getSortedItems() {
-    if (this.sorted) {
-      // iterate through items[]], create new array containing only the text property of each object
-      this.inputTextResorted = this.items.map(item => item.text);
-    }
+    // if (this.sorted) {
+      // iterate through items[], create new array containing only the text property of each object
+      this.inputTextResorted = this.items.map(item => item.text).join('\n').split('\n');
+      this.buttonText = 'Publish';
+      this.items[0].disabled = this.items[1].disabled = this.items[2].disabled = true;
+      console.log(this.inputTextResorted);
+    // }
   }
 
-  getValue(val1:string, val2:string, val3:string) {
-    console.warn(val1);
-    this.displayVal = val1 + '\n' + val2 + '\n' + val3;
+  hideResortableList(form: HTMLFormElement) {
+    form.hidden = true;
   }
+
   getTitle(val:string) {
     this.displayTitle = val;
   }
--- src/app/create-original-work/create-original-work.component.ts
+++ src/app/create-original-work/create-original-work.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-// import { DragDropModule } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Output, Input  } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-create-original-work',
@@ -7,27 +9,123 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-original-work.component.css']
 })
 export class CreateOriginalWorkComponent {
-  email = '';
-  firstName= '';
-  myText: string = 'My Component is ready hihi';
+  title = 'fyp';
+  workTitle: string = 'A long journey from bush to concrete';
+  i = 0;
 
-  title:string = "this is my title";
+  view = 0;
+  views = ['Interaction', 'Community', 'Public', 'Owner'];
+  dropdownActive = false;
 
-  sortableList = [
-    'Box 1',
-    'Box 2',
-    'Box 3',
-    'Box 4',
-    'Box 5',
+  paragraph: any = undefined;
+
+  user: string | undefined = undefined;
+  input: string = " ";
+  displayVal= '';
+  displayTitle = '';
+  rewritingSection1 = '';
+  rewritingSection2 = '';
+  sorted = false;
+  inputTextResorted : string[] = [];
+  @Output() viewSelected = new EventEmitter<string>();
+
+  items = [
+    { id: 'text1', rows: '8', cols: '38', placeholder: 'Enter 1st section of rewriting work', text: this.rewritingSection1 },
+    { id: 'text2', rows: '1', cols: '38', text: this.workTitle },
+    { id: 'text3', rows: '8', cols: '38', placeholder: 'Enter 2nd section of rewriting work', text: this.rewritingSection2 }
   ];
+  example_paragraph = document.getElementById('paragraph');
+  text = "Through decades that ran like rivers, endless rivers of endless woes. "
+  metadata = [
+    { index_interval_start: 0, index_interval_end: 5, revealed_score: 1 },
+    { index_interval_start: 6, index_interval_end: this.text.length, revealed_score: 0 },
+  ];
+  
+  get output(): string {
+    let spanIndex = 0;
+    let output = '';
+    this.metadata.forEach((data) => {
+      // add visible span
+      if (data.revealed_score === 1) {
+        const visibleSpan = `<span class="visible">${this.text.slice(data.index_interval_start, data.index_interval_end)}</span>`;
+        output += visibleSpan;
+      }
+      // add hidden span
+      if (data.revealed_score === 0) {
+        const hiddenSpan = `<span class="hidden">${this.text.slice(data.index_interval_start, data.index_interval_end)}</span>`;
+        output += hiddenSpan;
+      }
+    });
+    return output;
+  }
 
-  // title = 'fyp';
-  // i = 0;
-  // user: string | undefined = undefined;
+  ngOnInit(): void {
+    this.getParagraph();
+  }
 
-  // getData() {
-  //   console.log('Hi Yun ' + this.i);
-  //   this.i++;
-  //   this.user = 'Yun Chu'
+  toggleDropdown() {
+    // Toggle the dropdown
+    this.dropdownActive = !this.dropdownActive
+    console.log(this.dropdownActive)
+  }
+
+  getParagraph() {
+    //console.log(getParagraph())
+    console.log('Getting paragraph')
+    fetch("/api/v1/get-paragraph", {
+        method: 'GET',
+        mode: 'cors'
+    }
+    )
+    .then((response) => response.json())
+    .then((data => {
+      this.paragraph = data;
+      console.log(data)
+    }));
+  }
+
+  setView(newView: number) {
+    // Close the dropdown
+    this.dropdownActive = false
+
+    // Update the view
+    this.view = newView
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    const draggedItem = this.items[event.previousIndex];
+    const targetItem = this.items[event.currentIndex];
+
+    // Remove the dragged item from the array
+    this.items.splice(event.previousIndex, 1);
+    // Insert the dragged item at the target index
+    this.items.splice(event.currentIndex, 0, draggedItem);
+
+    // If the target item is defined, swap text values of the dragged item and the target item
+    if (targetItem) {
+      const temp = draggedItem.text;
+      draggedItem.text = targetItem.text;
+      targetItem.text = temp;
+    }
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+    this.sorted = true;
+  }
+
+  getSortedItems() {
+    // if (this.sorted) {
+      // iterate through items[], create new array containing only the text property of each object
+      this.inputTextResorted = this.items.map(item => item.text);
+      console.log(this.inputTextResorted);
+    // }
+  }
+
+  // getValue(val1:string, val2:string, val3:string) {
+  //   console.warn(val1);
+  //   this.displayVal = val1 + '\n' + val2 + '\n' + val3;
   // }
+  getTitle(val:string) {
+    this.displayTitle = val;
+  }
 }
+
+
